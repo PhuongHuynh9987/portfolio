322fd2583c2206523db27fd6ecb90ddaad1c99b5
Phuong Huynh <huynhphuong9987@gmail.com>
2024-01-17 14:42:25 -0800

    modified

--- scripts.js
+++ scripts.js
@@ -11,5 +11,5 @@ var x = document.getElementById("Ethereum_link");
 
 x.addEventListener("click", function(){
     document.getElementById("portfolio").style.display = "none";
-    document.getElementById("Ethereum").style.display = "grid";
+    document.getElementById("Ethereum").style.display = "block";
 });
